3deb123d7ef76da85d6eb0f58b63e097d446983e
mariamandafm <amanda.morais42@hotmail.com>
2023-01-22 20:50:12 -0300

    add server and mobile folder

---
+++ server/src/server.ts
@@ -0,0 +1,15 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { appRoutes } from './routes'
+
+const app = Fastify()
+
+app.register(cors)
+app.register(appRoutes)
+
+
+app.listen({
+    port: 3333, host: '192.168.1.12'
+}).then(() => {
+    console.log('HTTP Server running')
+})
